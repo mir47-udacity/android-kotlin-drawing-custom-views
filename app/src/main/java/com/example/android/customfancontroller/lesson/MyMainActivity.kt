1ac851afbb712403cb852d9fb7ea4082ed5511cb
miro <minev.miro@gmail.com>
2021-02-07 21:43:05 +0200

    create custom view

---
+++ app/src/main/java/com/example/android/customfancontroller/lesson/MyMainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.android.customfancontroller.lesson
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android.customfancontroller.R
+
+class MyMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_main)
+    }
+}
